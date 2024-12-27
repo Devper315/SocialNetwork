3f53a51165ae65cba9eb04adb5d02f9ab7711b85
Devper315 <Phanvanthi315@gmail.com>
2024-12-27 07:35:00 +0700

    update báo cáo

--- SocialNetworkBackend/src/main/java/com/social/network/SocialNetworkApp.java
+++ SocialNetworkBackend/src/main/java/com/social/network/SocialNetworkApp.java
@@ -3,14 +3,13 @@ package com.social.network;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
-public class SocialNetworkApplication {
+public class SocialNetworkApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SocialNetworkApplication.class, args);
+        SpringApplication.run(SocialNetworkApp.class, args);
     }
 
 }
